00f88e1a47d2f01557e34795ea3cd2fc67d4ce73
Sebastián Figueroa <sebaj348@gmail.com>
2025-07-26 17:11:35 -0400

    Added NotFoundPage

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <main className="w-full h-screen grid place-items-center bg-base">
+      <h1 className="text-2xl">404 - Page not found</h1>
+    </main>
+  );
+}
