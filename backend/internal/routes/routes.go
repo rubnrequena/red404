ee25ca7b8a0b234f2e9b8fef56faad629f89f79f
jose <joselgraterolc.8@gmail.com>
2025-07-27 21:20:59 -0400

    auth module added, so auth with jwt Bearer

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -4,25 +4,31 @@ import (
 	"net/http"
 
 	"github.com/escuadron-404/red404/backend/internal/handlers"
+	"github.com/escuadron-404/red404/backend/pkg/middleware"
 )
 
 // SetupRoutes configures all application routes.
-// It returns a http.Handler that wraps all defined routes.
-func SetupRoutes(userHandler *handlers.UserHandler) http.Handler { // Changed parameter
+func SetupRoutes(userHandler *handlers.UserHandler, authHandler *handlers.AuthHandler, authMiddleware *middleware.AuthMiddleware) http.Handler {
 	mux := http.NewServeMux()
 
-	// Register user-related routes
-	UserRoutes(mux, userHandler) // Removed db parameter
+	// Register authentication-related routes
+	AuthRoutes(mux, authHandler)
 
-	// add more routes here as needed
+	// Register user-related routes
+	UserRoutes(mux, userHandler, authMiddleware)
 
 	return mux
 }
 
-func UserRoutes(mux *http.ServeMux, userHandler *handlers.UserHandler) { // Removed db parameter
-	mux.HandleFunc("POST /api/users", userHandler.CreateUser)
-	mux.HandleFunc("GET /api/users/{id}", userHandler.GetUserByID)
-	mux.HandleFunc("GET /api/users", userHandler.GetAllUsers)
-	mux.HandleFunc("PUT /api/users/{id}", userHandler.UpdateUser)
-	mux.HandleFunc("DELETE /api/users/{id}", userHandler.DeleteUser)
+func AuthRoutes(mux *http.ServeMux, authHandler *handlers.AuthHandler) {
+	mux.HandleFunc("POST /api/login", authHandler.Login)
+	mux.HandleFunc("POST /api/register", authHandler.Register)
+}
+
+func UserRoutes(mux *http.ServeMux, userHandler *handlers.UserHandler, authMiddleware *middleware.AuthMiddleware) {
+	// mux.HandleFunc("POST /api/users", authMiddleware.Auth(userHandler.CreateUser))
+	mux.HandleFunc("GET /api/users/{id}", authMiddleware.Auth(userHandler.GetUserByID))
+	mux.HandleFunc("GET /api/users", authMiddleware.Auth(userHandler.GetAllUsers))
+	// mux.HandleFunc("PUT /api/users/{id}", authMiddleware.Auth(userHandler.UpdateUser))
+	// mux.HandleFunc("DELETE /api/users/{id}", authMiddleware.Auth(userHandler.DeleteUser))
 }
